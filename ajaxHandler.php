dcd74500ca48f15e2a61e979019eb2294b3303ed
nik7 <nik7007@users.noreply.github.com>
2015-06-23 19:49:50 +0200

    - add more control

--- ajaxHandler.php
+++ ajaxHandler.php
@@ -3,52 +3,55 @@ require_once("./init.php");
 
 require_once "./function/functions.php";
 
+if (isset($_POST["action"]))
+    switch (sanitizeString($_POST["action"])) {
 
-switch (sanitizeString($_POST["action"])) {
+        case "printRegistrableActivities":
+            if (!logged())
+                die("Reload");
 
-    case "printRegistrableActivities":
-        if (!logged())
-            die("Reload");
+            $page = false;
+            if (isset($_POST["page"]) && $_POST["page"] > 0)
+                $page = $_POST["page"];
+            printRegistrableActivities(getActivities($page, $_SESSION["user"]));
+            break;
 
-        $page = false;
-        if (isset($_POST["page"]) && $_POST["page"] > 0)
-            $page = $_POST["page"];
-        printRegistrableActivities(getActivities($page, $_SESSION["user"]));
-        break;
+        case "registerNewActivity":
+            if (!logged())
+                die("Reload");
+            if (isset($_POST["activity"]) && isset($_POST["number"]))
+                echo registerNewActivity($_POST["activity"], (int)$_POST["number"]);
+            else echo "Error! unable to satisfy the request.";
 
-    case "registerNewActivity":
-        if (!logged())
-            die("Reload");
-        if (isset($_POST["activity"]) && isset($_POST["number"]))
-            echo registerNewActivity($_POST["activity"], (int)$_POST["number"]);
-        else echo "Error! unable to satisfy the request.";
+            break;
 
-        break;
+        case "printCancelableActivities":
+            if (!logged())
+                die("Reload");
+            $page = false;
+            if (isset($_POST["page"]) && $_POST["page"] > 0)
+                $page = $_POST["page"];
+            printCancelableActivities(getActivities($page, $_SESSION["user"], true));
+            break;
 
-    case "printCancelableActivities":
-        if (!logged())
-            die("Reload");
-        $page = false;
-        if (isset($_POST["page"]) && $_POST["page"] > 0)
-            $page = $_POST["page"];
-        printCancelableActivities(getActivities($page, $_SESSION["user"], true));
-        break;
+        case "cancelRegister":
+            if (!logged())
+                die("Reload");
+            if (isset($_POST["activity"]))
+                cancelRegister($_POST["activity"], $_SESSION["user"]);
+            else
+                echo "Error! unable to satisfy the request.";
+            break;
+        case "closingPage":
+            closingPage();
+            break;
 
-    case "cancelRegister":
-        if (!logged())
-            die("Reload");
-        if (isset($_POST["activity"]))
-            cancelRegister($_POST["activity"], $_SESSION["user"]);
-        else
-            echo "Error! unable to satisfy the request.";
-        break;
-    case "closingPage":
-        closingPage();
-        break;
-
-    default:
-        die();
+        default:
+            closingPage();
+            die();
 
 
-}
+    }
+
+closingPage();
 
