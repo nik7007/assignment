14c4a91f5ce03e2572ea193d3a70fc6da995f522
nik7007 <nik7007@users.noreply.github.com>
2015-06-25 10:19:23 +0200

    - Remove ie9 compatibility

--- include.php
+++ include.php
@@ -1,9 +1,7 @@
 <?php
 require_once("./function/functions.php");
 ?>
-<noscript>Javascript is not supported by your browser, or is disabled. Please turn it on.</noscript>
+<noscript><p style="color: #ffffff; background-color: #e51a31; margin: 0; padding: 0 0 0 5px; position: fixed; width: 100%;">Javascript is not supported by your browser, or is disabled. Please turn it on.</p><br/></noscript>
 <script type="text/javascript" src="jQuery/jquery-1.11.3.js"></script>
 <script type="text/javascript" src="JS/myScript.js"></script>
-<link href="./CSS/style.css" rel="stylesheet" type="text/css"><!--[if lt IE 9]>
-<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-<![endif]-->
+<link href="./CSS/style.css" rel="stylesheet" type="text/css">
